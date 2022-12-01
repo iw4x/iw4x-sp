7681f64def99938988da4abca59679b28c91e52b
FutureRave <edoardo.sanguineti222@gmail.com>
2022-12-01 12:52:34 +0000

    maint: add start sp client script

---
+++ scripts/!start_sp_client.bat
@@ -0,0 +1,14 @@
+@echo off
+:: IW4x-SP Client Configuration start-up file
+
+:: Either put the batch in the IW4x-SP install dir, or change the value below to the IW4x-SP install dir
+set XLABS_MW2_INSTALL=%~dp0
+
+:: Remove this line, if automatic updates on start should be disabled
+start /W xlabs.exe -update
+
+:: You're done!! WARNING!!! Don't mess with anything below this line
+
+set GAME_EXE=%localappdata%\xlabs\data\iw4x\iw4x-sp.exe
+
+start %GAME_EXE%
