099d3a0f9351e88c4c24a594307fa0aabe7b04ca
FutureRave <edoardo.sanguineti222@gmail.com>
2023-05-01 11:52:44 +0100

    fix(network): fix this again

--- src/client/component/network.cpp
+++ src/client/component/network.cpp
@@ -1,11 +1,28 @@
 #include <std_include.hpp>
 #include "loader/component_loader.hpp"
 
+#include "command.hpp"
 #include "network.hpp"
 
 #include <utils/hook.hpp>
+#include <utils/string.hpp>
 
 namespace network {
+namespace {
+void cl_co_op_connect(const command::params& params) {
+  // Valid format is IP Port
+  if (params.size() < 3) {
+    return;
+  }
+
+  const auto input = std::strtol(params.get(2), nullptr, 10);
+  const auto port = ::htons(static_cast<std::uint16_t>(input));
+
+  command::execute(
+      utils::string::va("connect_coop {0} {1}", params.get(1), port));
+}
+} // namespace
+
 class component final : public component_interface {
 public:
   void post_load() override { patch_sp(); }
@@ -40,9 +57,9 @@ private:
     utils::hook(0x4BF4D1, game::ShortNoSwap, HOOK_CALL).install()->quick();
     utils::hook(0x4BF50C, game::ShortNoSwap, HOOK_CALL).install()->quick();
 
-    // Disable BigShort in NET_StringToAdr
-    utils::hook(0x40A657, game::ShortNoSwap, HOOK_CALL).install()->quick();
-    utils::hook(0x40A676, game::ShortNoSwap, HOOK_CALL).install()->quick();
+    // Fix NET_StringToAdr (Only known workaround that actually works)
+    utils::hook::set<const char*>(0x475417, "connect_coop");
+    command::add("connect", cl_co_op_connect);
 
     // Disable BigShort in GetLocalAddressForEncryptedConnection
     utils::hook(0x4CF42D, game::ShortNoSwap, HOOK_CALL).install()->quick();
