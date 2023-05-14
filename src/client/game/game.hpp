61015511765118eb5e47ed976f29768aad070cd0
FutureRave <edoardo.sanguineti222@gmail.com>
2023-05-14 16:57:39 +0100

    maint(game): use new symbol class

--- src/client/game/game.hpp
+++ src/client/game/game.hpp
@@ -1,19 +1,28 @@
 #pragma once
 
 namespace game {
-
-template <typename T> class symbol {
+template <typename T> class base_symbol {
 public:
-  symbol(const size_t sp) : sp_(reinterpret_cast<T*>(sp)) {}
+  base_symbol(const std::size_t address) : address_(address) {}
 
-  [[nodiscard]] T* get() const { return sp_; }
+  [[nodiscard]] T* get() const { return reinterpret_cast<T*>(this->address_); }
 
   operator T*() const { return this->get(); }
 
   T* operator->() const { return this->get(); }
 
 private:
-  T* sp_;
+  std::size_t address_{};
+};
+
+template <typename T> struct symbol : base_symbol<T> {
+  using base_symbol<T>::base_symbol;
+};
+
+template <typename T, typename... Args>
+struct symbol<T(Args...)> : base_symbol<T(Args...)> {
+  using func_type = T(Args...);
+  using base_symbol<func_type>::base_symbol;
 };
 
 // Functions that require asm stubs to be called
